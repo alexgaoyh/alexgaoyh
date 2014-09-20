d44b97832f3767899b390fab956a439dfb78157d
alexgaoyh <alexgaoyh@sina.com>
2014-09-20 17:09:13 +0800

    baseController add common||general list method for admin

--- src/main/java/com/alexgaoyh/common/action/BaseController.java
+++ src/main/java/com/alexgaoyh/common/action/BaseController.java
@@ -1,7 +1,35 @@
 package com.alexgaoyh.common.action;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.alexgaoyh.common.entity.BaseEntity;
 
 public class BaseController<E extends BaseEntity> {
 
+	/**
+	 * 后台list页面
+	 * 如请求地址为：   	http://localhost:8080/web/sysmanRole/list
+	 * 则返回的页面应该在    /web/WEB-INF/views/sysmanRole/list.jsp
+	 * @return
+	 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		RequestMapping rm = this.getClass().getAnnotation(RequestMapping.class);
+		String moduleName = "";
+		if (rm != null) {
+			String[] values = rm.value();
+			if (ArrayUtils.isNotEmpty(values)) {
+				moduleName = values[0];
+			}
+		}
+		if (moduleName.endsWith("/")) {
+			moduleName = moduleName.substring(0, moduleName.length() - 1);
+		}
+		System.out.println(moduleName);
+		return new ModelAndView("views/" + moduleName + "/list");
+	}
+	
 }
