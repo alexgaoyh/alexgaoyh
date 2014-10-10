16ce93f3989dfeac7dd48cd2d69f10d23bcacecb
alexgaoyh <alexgaoyh@sina.com>
2014-10-10 13:53:44 +0800

    utf-8

--- src/main/java/com/alexgaoyh/unbusiness/freemarker/GenerateJavaFile.java
+++ src/main/java/com/alexgaoyh/unbusiness/freemarker/GenerateJavaFile.java
@@ -9,7 +9,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
@@ -17,28 +16,25 @@ import freemarker.template.Template;
 /**
  * 通过设定参数值，生成对应的action dao entity service 对应的java类
  * 修改demo()方法内部的root.put()的value值，运行即可
+ * 
  * @author gaoyihang
  *
  */
 public class GenerateJavaFile {
-
 	public static void main(String[] args) {
 		demo();
 	}
 
 	public static void demo() {
-
 		Map<String, Object> root = new HashMap<String, Object>();
-		root.put("packageName", "com.alexgaoyh.aaaaaaaaa.admin");//包路径
-		root.put("className", "SysmanRole");//类名称
-		root.put("springName", "sysmanRole");//类名称的首字母小写
-		root.put("author", "alexgaoyh");//作者
-		root.put("tableName", "SYSMAN_ROLE");//表名称
-		root.put("entityNameCN", "用户后台登陆用户角色表--RBAC权限管理");//类描述
-		root.put("date", new Date().toString());//创建时间
-		
+		root.put("packageName", "com.alexgaoyh.aaaaaaaaa.admin");// 包路径
+		root.put("className", "SysmanRole");// 类名称
+		root.put("springName", "sysmanRole");// 类名称的首字母小写
+		root.put("author", "alexgaoyh");// 作者
+		root.put("tableName", "SYSMAN_ROLE");// 表名称
+		root.put("entityNameCN", "用户后台登陆用户角色表--RBAC权限管理");// 类描述
+		root.put("date", new Date().toString());// 创建时间
 		String workDir = (String) System.getProperties().get("user.dir");
-
 		try {
 			entity(workDir, root);
 			dao(workDir, root);
@@ -51,89 +47,85 @@ public class GenerateJavaFile {
 		}
 	}
 
-	public static void entity(String workDir, Map<String, Object> input) throws Exception {
-
+	public static void entity(String workDir, Map<String, Object> input)
+			throws Exception {
 		String fileName = workDir + "/src/main/java/"
 				+ input.get("packageName").toString().replaceAll("\\.", "/")
 				+ "/entity/" + input.get("className").toString() + ".java";
-		
 		File myFile = new File(fileName);
 		myFile.getParentFile().mkdirs();
 		myFile.createNewFile();
-
 		buildFile("templete/entity.ftl", fileName, input);
 	}
-	
-	public static void dao(String workDir, Map<String, Object> input) throws Exception {
 
+	public static void dao(String workDir, Map<String, Object> input)
+			throws Exception {
 		String fileName = workDir + "/src/main/java/"
 				+ input.get("packageName").toString().replaceAll("\\.", "/")
 				+ "/dao/" + input.get("className").toString() + "Dao.java";
-		
 		File myFile = new File(fileName);
 		myFile.getParentFile().mkdirs();
 		myFile.createNewFile();
-
 		buildFile("templete/dao.ftl", fileName, input);
 	}
-	
-	public static void daoImpl(String workDir, Map<String, Object> input) throws Exception {
 
+	public static void daoImpl(String workDir, Map<String, Object> input)
+			throws Exception {
 		String fileName = workDir + "/src/main/java/"
 				+ input.get("packageName").toString().replaceAll("\\.", "/")
-				+ "/dao/impl/" + input.get("className").toString() + "DaoImpl.java";
-		
+				+ "/dao/impl/" + input.get("className").toString()
+				+ "DaoImpl.java";
 		File myFile = new File(fileName);
 		myFile.getParentFile().mkdirs();
 		myFile.createNewFile();
-
 		buildFile("templete/daoImpl.ftl", fileName, input);
 	}
-	
-	public static void service(String workDir, Map<String, Object> input) throws Exception {
 
+	public static void service(String workDir, Map<String, Object> input)
+			throws Exception {
 		String fileName = workDir + "/src/main/java/"
 				+ input.get("packageName").toString().replaceAll("\\.", "/")
-				+ "/service/" + input.get("className").toString() + "Service.java";
-		
+				+ "/service/" + input.get("className").toString()
+				+ "Service.java";
 		File myFile = new File(fileName);
 		myFile.getParentFile().mkdirs();
 		myFile.createNewFile();
-
 		buildFile("templete/service.ftl", fileName, input);
 	}
-	
-	public static void serviceImpl(String workDir, Map<String, Object> input) throws Exception {
 
+	public static void serviceImpl(String workDir, Map<String, Object> input)
+			throws Exception {
 		String fileName = workDir + "/src/main/java/"
 				+ input.get("packageName").toString().replaceAll("\\.", "/")
-				+ "/service/impl/" + input.get("className").toString() + "ServiceImpl.java";
-		
+				+ "/service/impl/" + input.get("className").toString()
+				+ "ServiceImpl.java";
 		File myFile = new File(fileName);
 		myFile.getParentFile().mkdirs();
 		myFile.createNewFile();
-
 		buildFile("templete/serviceImpl.ftl", fileName, input);
 	}
-	
-	public static void action(String workDir, Map<String, Object> input) throws Exception {
 
+	public static void action(String workDir, Map<String, Object> input)
+			throws Exception {
 		String fileName = workDir + "/src/main/java/"
 				+ input.get("packageName").toString().replaceAll("\\.", "/")
-				+ "/action/" + input.get("className").toString() + "Action.java";
-		
+				+ "/action/" + input.get("className").toString()
+				+ "Action.java";
 		File myFile = new File(fileName);
 		myFile.getParentFile().mkdirs();
 		myFile.createNewFile();
-
 		buildFile("templete/action.ftl", fileName, input);
 	}
 
 	/**
 	 * 生成文件
-	 * @param templateName 模板文件
-	 * @param fileName 生成文件
-	 * @param root 参数
+	 * 
+	 * @param templateName
+	 *            模板文件
+	 * @param fileName
+	 *            生成文件
+	 * @param root
+	 *            参数
 	 */
 	private static void buildFile(String templateName, String fileName, Map root) {
 		Configuration freemarkerCfg = new Configuration();
@@ -144,7 +136,8 @@ public class GenerateJavaFile {
 			template = freemarkerCfg.getTemplate(templateName);
 			template.setEncoding("UTF-8");
 			File htmlFile = new File(fileName);
-			Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(htmlFile), "UTF-8"));
+			Writer out = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(htmlFile), "UTF-8"));
 			template.process(root, out);
 			out.flush();
 		} catch (Exception e) {
